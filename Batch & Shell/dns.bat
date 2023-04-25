3a156cda40ffa1f3463e246467ca2e0f49ce22c4
Simon Haubrich <simon.haubrich@protonmail.com>
2023-04-25 16:11:55 +0200

    Update and rename change_dns.bat to dns.bat

---
+++ Batch & Shell/dns.bat
@@ -0,0 +1,49 @@
+REM Created 25.04.2023
+REM Changes the IP, Subnet, Gateway and primary and secondary DNS Servers
+REM Made for the latest Windows 10 Version
+@echo off
+title Netzwerkeinstellungen setzen
+
+setlocal enabledelayedexpansion
+
+REM DNS Variablen
+set primaryDNS=192.168.1.1
+set secondaryDNS=192.168.1.254
+set ip_addr=192.168.1.83
+set custom_subnet=255.255.255.0
+set custom_gateway=192.168.1.254
+
+echo Please choose:
+echo 1) Custom (static IP Address)
+echo 2) DHCP (dynamic IP Address)
+echo 3) Exit
+
+set /p choice=
+
+REM Choice 1 = Custom
+if "%choice%"=="1" (
+    echo Configuring Network...
+    REM 1: IP Adresse; 2: Subnetzmaske; 3: Standardgateway
+    netsh interface ipv4 set address name="Ethernet" static %ip_addr% %custom_subnet% %custom_gateway%
+    netsh interface ipv4 set dns name="Ethernet" static %primaryDNS% primary
+    netsh interface ipv4 add dns name="Ethernet" %secondaryDNS% index=2
+    echo Finished.
+    
+REM Choice 2 = DHCP
+) else if "%choice%"=="2" (
+    echo Configuring Network...
+    REM DNS und IP Adresse werden über DHCP bezogen
+    netsh interface ip set address name="Ethernet" source=dhcp
+    netsh interface ip set dns name="Ethernet" source=dhcp
+    netsh interface ip set address name="Wi-Fi" source=dhcp
+    netsh interface ip set dns name="Wi-Fi" source=dhcp
+    echo Finished.
+
+REM Choice 3 = Exit
+) else if "%choice%"=="3" (
+    echo Exiting...
+    exit
+) else (
+    echo Wrong choice. Please enter 1, 2 or 3.
+    pause
+)
